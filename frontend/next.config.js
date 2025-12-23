ad16f6cc10f4243ae15f34c77feae073493c817b
minsol0302 <pjjw1233@gmail.com>
2025-12-23 13:12:26 +0900

    aws 도메인 연결2차

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,19 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   // 환경 변수는 NEXT_PUBLIC_ 접두사가 있으면 자동으로 클라이언트에 노출됨
   // next.config.js의 env 섹션은 선택사항
+
+  webpack: (config) => {
+    // tsconfig.json의 paths 설정과 일치하도록 webpack alias 설정
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
